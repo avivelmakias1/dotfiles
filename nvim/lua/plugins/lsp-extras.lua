7cbd05f485576e226a41ff96739ae7a63fde57ad
Aviv Elmakias <aviv.elmakias.b@gmail.com>
2023-02-17 17:02:14 +0100

    adding lsp-zero for easy config

--- nvim/lua/plugins/lsp-extras.lua
+++ nvim/lua/plugins/lsp-extras.lua
@@ -14,7 +14,26 @@ return {
     },
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
+    dependencies = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lua" },
+
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
