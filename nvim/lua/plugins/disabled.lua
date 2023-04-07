94b3933a85fa87aab610dd9c300bd18bf60be496
Aviv Elmakias <aviv.elmakias.b@gmail.com>
2023-04-07 21:16:59 +0300

    removed all nvim old stuff

--- nvim/lua/plugins/disabled.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "echasnovski/mini.animate",
-  enabled = false,
-}
--- nvim/lua/plugins/toggleterm.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  opts = function(_, opts)
-    opts.open_mapping = [[<c-\>]]
-  end,
-}
