bc359bcd8da80896276c3828634e58428835a5d3
aviv-elmakias <aviv.elmakias@grip.security>
2024-02-13 14:19:32 +0200

    add tests with neotest

---
+++ nvim/.config/nvim/lua/custom/plugins/tests.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-python',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-python',
+        },
+      }
+    end,
+  },
+}
