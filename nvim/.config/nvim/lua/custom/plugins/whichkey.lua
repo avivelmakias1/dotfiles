6134be3d5c2b8d6ba5e1c7864c3ed0f2154417c0
aviv-elmakias <aviv.elmakias@grip.security>
2024-07-18 18:47:16 +0300

    updating which key to newest version

--- nvim/.config/nvim/lua/custom/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/custom/plugins/whichkey.lua
@@ -7,20 +7,19 @@ return {
     config = function(_, opts)
       local wk = require 'which-key'
       wk.setup(opts)
-      local keymaps = {
-        mode = { 'n', 'v' },
-        ['<leader>c'] = { name = '+code' },
-        ['<leader>d'] = { name = '+debug' },
-        ['<leader>b'] = { name = '+buffers' },
-        ['<leader>f'] = { name = '+find' },
-        ['<leader>fi'] = { name = '+find in' },
-        ['<leader>g'] = { name = '+git' },
-        ['<leader>w'] = { name = '+workspaces' },
-        ['<leader>q'] = { name = '+diagnostics' },
-        ['<leader>l'] = { name = '+lsp' },
-        ['<leader>t'] = { name = '+toggles' },
+      wk.add {
+        { '<leader>c', group = '+code' },
+        { '<leader>c', group = '+code' },
+        { '<leader>d', group = '+debug' },
+        { '<leader>b', group = '+buffers' },
+        { '<leader>f', group = '+find' },
+        { '<leader>fi', group = '+find in' },
+        { '<leader>g', group = '+git' },
+        { '<leader>w', group = '+workspaces' },
+        { '<leader>q', group = '+diagnostics' },
+        { '<leader>l', group = '+lsp' },
+        { '<leader>t', group = '+toggles' },
       }
-      wk.register(keymaps)
     end,
   },
 }
