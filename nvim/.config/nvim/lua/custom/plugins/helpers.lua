a18cea02b108f2ff34bf20b6ef9f7d3c9c73e463
aviv-elmakias <aviv.elmakias@grip.security>
2024-08-04 18:08:45 +0300

    no need for better escape, as i use jk often

--- nvim/.config/nvim/lua/custom/plugins/helpers.lua
+++ nvim/.config/nvim/lua/custom/plugins/helpers.lua
@@ -18,13 +18,6 @@ return {
     },
   },
   {
-    'max397574/better-escape.nvim',
-    event = 'InsertEnter',
-    config = function()
-      require('better_escape').setup()
-    end,
-  },
-  {
     'gbprod/cutlass.nvim',
   },
   {
