63e22c6f2cb3a4dd7c16e36671156d951da4b007
aviv-elmakias <aviv.elmakias@grip.security>
2024-09-23 09:24:47 +0300

    remove the esc esc so i can use lazygit

--- nvim/.config/nvim/after/plugin/mappings.lua
+++ nvim/.config/nvim/after/plugin/mappings.lua
@@ -14,7 +14,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
