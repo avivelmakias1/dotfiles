99ac7e9626aec467935f58fe198caeee4d830eea
aviv-elmakias <aviv.elmakias@grip.security>
2024-12-29 16:44:53 +0200

    linting without null ls

--- nvim/.config/nvim/lua/custom/plugins/lint.lua
+++ nvim/.config/nvim/lua/custom/plugins/lint.lua
@@ -33,8 +33,10 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        fish = { 'fish_indent' },
+        sh = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
