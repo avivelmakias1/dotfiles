b47ef77c4d8940255e793f76d9186b646389ae9c
aviv-elmakias <aviv.elmakias@grip.security>
2025-02-03 17:45:47 +0200

    adding a way to remove autoformat

--- nvim/.config/nvim/lua/custom/plugins/lint.lua
+++ nvim/.config/nvim/lua/custom/plugins/lint.lua
@@ -16,6 +16,9 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -27,19 +30,19 @@ return {
           lsp_format_opt = 'fallback'
         end
         return {
-          timeout_ms = 500,
+          timeout_ms = 5000,
           lsp_format = lsp_format_opt,
         }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        fish = { 'fish_indent' },
         sh = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        sql = { 'sqlfmt' },
       },
     },
   },
