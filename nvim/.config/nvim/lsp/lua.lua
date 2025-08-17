c2aaa8d068e4b554edb5e074533202372963664b
aviv-elmakias <aviv.elmakias@grip.security>
2025-08-17 12:27:02 +0300

    fix lua

--- nvim/.config/nvim/lsp/lua.lua
+++ nvim/.config/nvim/lsp/lua.lua
@@ -1,13 +1,32 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { "lua-language-server" },
-    filetypes = { "lua" },
-    root_markers = { ".luarc.json", ".luarc.jsonc" },
-    telemetry = { enabled = false },
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = {
+        '.luarc.json',
+        '.luarc.jsonc',
+        '.luacheckrc',
+        '.stylua.toml',
+        'stylua.toml',
+        'selene.toml',
+        'selene.yml',
+        '.git',
+    },
     settings = {
         Lua = {
             runtime = {
-                version = "LuaJIT",
+                version = "Lua 5.4",
+            },
+            completion = {
+                enable = true,
+            },
+            diagnostics = {
+                enable = true,
+                globals = { "vim" },
+            },
+            workspace = {
+                library = { vim.env.VIMRUNTIME },
+                checkThirdParty = false,
             },
         },
     },
