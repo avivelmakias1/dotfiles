b535d5f9a41f0c928a389516712cf48069c854ea
aviv-elmakias <aviv.elmakias@grip.security>
2025-08-26 19:54:56 +0300

    adding copilot

---
+++ nvim/.config/nvim/lua/aviv/plugins/lsp/ai.lua
@@ -0,0 +1,22 @@
+return {
+  'zbirenbaum/copilot.lua',
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        trigger_on_accept = true,
+        keymap = {
+          accept = '<C-l>',
+          accept_word = false,
+          accept_line = false,
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+      },
+    }
+  end,
+}
